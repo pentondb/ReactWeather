2871b820d5002ab23adf64a8759c611fcf6e12eb
pentondb <derek.penton@gmail.com>
2018-11-11 17:58:25 -0600

    Removed commented out code in stateless functional components

--- app/components/WeatherMsg.jsx
+++ app/components/WeatherMsg.jsx
@@ -1,27 +1,5 @@
 var React = require('react');
 
-// var WeatherMsg = React.createClass({
-//     render: function () {
-//         var { temp, location } = this.props;
-//         return (
-//             <p>It's {temp} in {location}</p>
-//         );
-//     }
-// });
-
-// var WeatherMsg = (props) => {
-//     var { temp, location } = props;
-//     return (
-//         <p>It's {temp} in {location}</p>
-//     );
-// };
-
-// var WeatherMsg = ({ temp, location }) => {
-//     return (
-//         <p>It's {temp} in {location}</p>
-//     );
-// };
-
 var WeatherMsg = ({ temp, location }) => <p>It's {temp} in {location}</p>;
 
 module.exports = WeatherMsg;
