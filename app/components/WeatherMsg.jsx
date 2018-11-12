1e2c29bde4e1892b60c8e3c01f2f0bd1ce33e2bd
pentondb <derek.penton@gmail.com>
2018-11-11 20:10:21 -0600

    removed text from stateless functional components

--- app/components/WeatherMsg.jsx
+++ app/components/WeatherMsg.jsx
@@ -1,5 +1,9 @@
 var React = require('react');
 
-var WeatherMsg = ({ temp, location }) => <p>It's {temp} in {location}</p>;
+var WeatherMsg = ({ temp, location }) => {
+    return (
+        <p>It's {temp} in {location}</p>
+    );
+};
 
 module.exports = WeatherMsg;
