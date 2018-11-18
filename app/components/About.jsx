a9e1de7b9f69782cf84a34e197c51d11cd945bcc
pentondb <derek.penton@gmail.com>
2018-11-18 15:49:30 -0600

    Style About page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,19 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
+            <h1 className="text-center">About</h1>
             <p>Welcome to the About page!</p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a>
+                </li>
+                <li>
+                    <a href="https://openweathermap.org">OpenWeatherMap</a>
+                </li>
+                <li>
+                    <a href="https://www.google.com">Google</a>
+                </li>
+            </ul>
         </div>
     );
 };
